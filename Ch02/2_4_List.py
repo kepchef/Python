76c5a7422a416991a45a1ca91483af3b214c001c
kepchef@naver.com <kepchef@naver.com>
2020-12-21 17:40:22 +0900

    2020-12-21

---
+++ Ch02/2_4_List.py
@@ -0,0 +1,41 @@
+"""
+날짜:2020/12/21
+이름:김은표
+내용:파이썬 자료구조 리스트 실습하기
+"""
+# 리스트
+list1 = [1, 2, 3, 4, 5]
+print('list[0] : ', list1[0])
+print('list[2] : ', list1[2])
+print('list[4] : ', list1[4])
+
+list2 = [1, 1.23, True, '홍길동']
+print('list2[0] : ', list2[0])
+print('list2[1] : ', list2[1])
+print('list2[2] : ', list2[2])
+print('list2[3] : ', list2[3])
+
+list3 =[[1, 2, 3],
+        [4, 5, 6],
+        [7, 8, 9]]
+
+print('list3[0] : ',list3[0])
+print('list3[0][1] : ',list3[0][1])
+print('list3[2][1] : ',list3[0][1])
+# 리스트 더하기
+animal1 = ['사자', '호랑이']
+animal2 = ['곰', '코끼리', '기린']
+animal3 = animal1 + animal2
+print('animal3 : ', animal3)
+
+# 리시트 수정, 변경, 삭제
+count = [1, 2, 3, 4, 5]
+
+count[1] = 7
+print('count : ', count)
+
+count[2:3] = [8, 9]
+print('count : ', count)
+
+count[1:3] = []
+print('count : ', count)
